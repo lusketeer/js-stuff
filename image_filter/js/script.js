e1038599d8bb355b1e1771a8e7eb9cc784eb3ef6
Luke <luke8520@gmail.com>
2015-09-04 14:27:40 +0200

    minor bugs

--- image_filter/js/script.js
+++ image_filter/js/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var filters = ["Original"].concat(Object.keys($.filterMe.filters));
   $.each(filters, function(index, filter) {
     var sampleImage = $("<img>").attr({ "src": "img/sample.png", "data-filter": filter});
+    sampleImage.filterMe();
     var filterElement = $("<a>").attr("href", "javascript:void(0)").addClass("filter-option thumbnail").html(sampleImage);
     var filterWrapper = $("<div>").addClass("col-sm-4").html(filterElement);
     // var filterElement = $("<a>").addClass("filter-option btn btn-success col-sm-4").html(filter);
@@ -12,7 +13,7 @@ $(document).ready(function() {
     $("a.filter-option").click(function() {
       var img = $("<img>").attr({"src": "img/72H.jpg", "data-filter": ""}).addClass("img-responsive filter");
       $("div.img-wrapper").html(img);
-      // var option = (this.text === "Original") ? "" : this.text;
+      var option = $(this).attr("data-filter");
       $(".filter").attr("data-filter", option);
       $(".filter").filterMe();
     });
