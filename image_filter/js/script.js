5341c13ac0fed72722548bb19e8a8f4a5c56991d
Luke <luke8520@gmail.com>
2015-09-04 14:12:25 +0200

    set time out 2s for each filter applies

--- image_filter/js/script.js
+++ image_filter/js/script.js
@@ -5,11 +5,13 @@ $(document).ready(function() {
     var filterElement = $("<a>").addClass("filter-option btn btn-success col-sm-4").html(filter);
     filterList.append(filterElement);
   });
-  $("a.filter-option").click(function() {
-    var img = $("<img>").attr({"src": "img/72H.jpg", "data-filter": ""}).addClass("img-responsive filter");
-    $("div.img-wrapper").html(img);
-    var option = (this.text === "Original") ? "" : this.text;
-    $(".filter").attr("data-filter", option);
-    $(".filter").filterMe();
-  });
+  setTimeout(function() {
+    $("a.filter-option").click(function() {
+      var img = $("<img>").attr({"src": "img/72H.jpg", "data-filter": ""}).addClass("img-responsive filter");
+      $("div.img-wrapper").html(img);
+      var option = (this.text === "Original") ? "" : this.text;
+      $(".filter").attr("data-filter", option);
+      $(".filter").filterMe();
+    });
+  }, 2000);
 })
