ccc87d372f1aae552e519151103ec3d4e22864f6
Luke <luke8520@gmail.com>
2015-09-04 13:46:27 +0200

    added more filter options

--- image_filter/js/script.js
+++ image_filter/js/script.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  var filterList = $("div.filter-list");
+  var filters = ["Original"].concat(Object.keys($.filterMe.filters));
+  $.each(filters, function(index, filter) {
+    var filterElement = $("<a>").addClass("filter-option btn btn-success col-sm-4").html(filter);
+    filterList.append(filterElement);
+  });
   $("a.filter-option").click(function() {
     var option = this.text;
     $(".filter").attr("data-filter", option);
