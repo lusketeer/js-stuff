96d4f7fd60f600ad81b140689bc28549f797fd69
Luke <luke8520@gmail.com>
2015-09-04 14:23:40 +0200

    change buttons to sample images with filter

--- image_filter/js/script.js
+++ image_filter/js/script.js
@@ -2,14 +2,17 @@ $(document).ready(function() {
   var filterList = $("div.filter-list");
   var filters = ["Original"].concat(Object.keys($.filterMe.filters));
   $.each(filters, function(index, filter) {
-    var filterElement = $("<a>").addClass("filter-option btn btn-success col-sm-4").html(filter);
-    filterList.append(filterElement);
+    var sampleImage = $("<img>").attr({ "src": "img/sample.png", "data-filter": filter});
+    var filterElement = $("<a>").attr("href", "javascript:void(0)").addClass("filter-option thumbnail").html(sampleImage);
+    var filterWrapper = $("<div>").addClass("col-sm-4").html(filterElement);
+    // var filterElement = $("<a>").addClass("filter-option btn btn-success col-sm-4").html(filter);
+    filterList.append(filterWrapper);
   });
   setTimeout(function() {
     $("a.filter-option").click(function() {
       var img = $("<img>").attr({"src": "img/72H.jpg", "data-filter": ""}).addClass("img-responsive filter");
       $("div.img-wrapper").html(img);
-      var option = (this.text === "Original") ? "" : this.text;
+      // var option = (this.text === "Original") ? "" : this.text;
       $(".filter").attr("data-filter", option);
       $(".filter").filterMe();
     });
